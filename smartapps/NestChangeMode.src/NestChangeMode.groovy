cf42fe32dd020128c4032ea8bebe5dc5a96c40d3
Yves Racine <yracine66@gmail.com>
2020-11-30 16:06:54 -0500

    v1.4

--- smartapps/NestChangeMode.src/NestChangeMode.groovy
+++ smartapps/NestChangeMode.src/NestChangeMode.groovy
@@ -1,7 +1,7 @@
 /**
  *  NestChangeMode
  *
- *  Copyright 2018 Yves Racine
+ *  Copyright Yves Racine
  *  LinkedIn profile: ca.linkedin.com/pub/yves-racine-m-sc-a/0/406/4b/
  *
  *  Developer retains all right, title, copyright, and interest, including all copyright, patent rights, trade secret 
@@ -34,7 +34,7 @@ definition(
 )
 
 private def get_APP_VERSION() {
-	return "1.3"
+	return "1.4"
 }    
 
 
@@ -49,23 +49,25 @@ preferences {
 			paragraph "If you like this smartapp, please support the developer via PayPal and click on the Paypal link below " 
 				href url:"https://www.paypal.me/ecomatiqhomes",
 					title:"Paypal donation..."
-			paragraph "Copyright©2018 Yves Racine"
+			paragraph "Copyright©2018-2020 Yves Racine"
 				href url:"http://github.com/yracine/device-type-myNext", style:"embedded", required:false, title:"More information...", 
 					description: "http://github.com/yracine"
 		}
         
 	}
 	page(name: "selectProgram", title: "Nest mode", content: "selectProgram")
-	page(name: "Notifications", title: "Notifications Options", install: true, uninstall: true) {
-		section("Notifications") {
-			input "sendPushMessage", "enum", title: "Send a push notification?", metadata: [values: ["Yes", "No"]], required:
-				false
-			input "phone", "phone", title: "Send a Text Message?", required: false
-		}
-        section([mobileOnly:true]) {
-			label title: "Assign a name for this SmartApp", required: false
-		}
+	page(name: "Notifications", title: "Notifications & other Options", install: true, uninstall: true) {
+        if (isST()) {        
+    		section("Notifications") {
+	    			input "sendPushMessage", "enum", title: "Send a push notification?", options:["Yes", "No"], required:
+			    		false
+    				input "phone", "phone", title: "Send a Text Message?", required: false
+	    		}
+        	}            
 	}
+	section([mobileOnly:true]) {
+        	label title: "Assign a name for this SmartApp", required: false
+    	}
 }
 
 
@@ -98,6 +100,20 @@ def selectProgram() {
 
         
 	}
+}
+
+boolean isST() { 
+    return (getHub() == "SmartThings") 
+}
+
+private getHub() {
+    def result = "SmartThings"
+    if(state?.hub == null) {
+        try { [value: "value"]?.encodeAsJson(); } catch (e) { result = "Hubitat" }
+        state?.hub = result
+    }
+    log.debug "hubPlatform: (${state?.hub})"
+    return state?.hub
 }
 
 
